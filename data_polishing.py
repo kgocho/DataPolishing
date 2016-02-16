258b2efe800a7eeb2eaa9f1fe52f0bf5b285a397
gocho <gocho@dais.is.tohoku.ac.jp>
2016-02-16 19:28:20 +0900

    Update data_polishing.py

--- data_polishing.py
+++ data_polishing.py
@@ -10,16 +10,18 @@ Created on 2016/02/05
 "Micro-Clustering: Finding Small Clusters in Large Diversity, Takeaki Uno et.al"
 """
 
+import numpy as np
+
 class DataPolishing:
     def __init__(self, Graph):
         self.Graph = Graph.simplify()   #グラフを単純グラフにする
         print "init Graph"
 
     def polish(self, vertex0, vertex1, sim, polish_ratio = 0.3):      #類似度を基に辺の取り外しを行う
-        e = self.Graph.es.select(_within=[vertex0, vertex1])          #vertex0とvertex1の間の辺
-        if(len(e) == 0 and sim[vertex0][vertex1] >= polish_ratio):    #辺がつながっていなくて類似度が大きい
+        are_connected_flag = self.Graph.are_connected(vertex0, vertex1) ##vertex0とvertex1の間に辺があるか
+        if(not(are_connected_flag) and sim[vertex0][vertex1] >= polish_ratio):    #辺がつながっていなくて類似度が大きい
             self.Graph.add_edge(vertex0, vertex1)
-        elif(len(e) > 0 and sim[vertex0][vertex1] < polish_ratio):    #辺がつながっていて類似度小さい
+        elif(are_connected_flag and sim[vertex0][vertex1] < polish_ratio):        #辺がつながっていて類似度小さい
             self.Graph.delete_edges((vertex0,vertex1))
 
     def data_polish_direct(self, polish_ratio = 0.3, loop = 30):      #directな方法
@@ -36,15 +38,32 @@ class DataPolishing:
         print "end", i+1 , "times"
 
     def data_polish(self, polish_ratio = 0.3, loop = 30):       #グラフがスパースであれば高速
-        for i in range(loop):
-            sim = self.Graph.similarity_jaccard()
+        for i in xrange(loop):
+            sim = self.jaccard()
             temp = self.Graph.get_edgelist()
-            for u in self.Graph.vs():
+            for u in xrange(self.Graph.vcount()):
                 for w in self.Graph.neighbors(u):
-                    for v in [x for x in self.Graph.neighbors(w) if x < u.index]:
-                        self.polish(u.index, v, sim, polish_ratio)
+                    for v in [x for x in self.Graph.neighbors(w) if x < u]:
+                        self.polish(u, v, sim, polish_ratio)
             if(temp == self.Graph.get_edgelist()):
                 print "break at " + str(i+1) + " times"
                 break
             #igraph.summary(self.Graph)
         print "end", i+1 , "times"
+
+    def jaccard(self):
+        sim = np.zeros([self.Graph.vcount(), self.Graph.vcount()])
+        intersection = [0] * self.Graph.vcount()
+        for u in xrange(self.Graph.vcount()):
+            L = []
+            for w in self.Graph.neighborhood(u):
+                for v in [x for x in self.Graph.neighborhood(w) if x <= u]:
+                    if(intersection[v] == 0):
+                        L.append(v)
+                    intersection[v] += 1
+            for v in L:
+                #print (v, u.index, intersection[v])
+                sim[v][u] = float(intersection[v]) / (self.Graph.neighborhood_size(v)+ self.Graph.neighborhood_size(u) - intersection[v])
+                sim[u][v] = sim[v][u]
+                intersection[v] = 0
+        return sim
